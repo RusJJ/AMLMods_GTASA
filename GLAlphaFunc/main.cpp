e3fac81d5b69ef8c2988342e976aec12b188aba1
RusJJ <rusjj1337@gmail.com>
2021-08-14 22:49:33 +0300

    Update main.cpp
    GLAlphaFunc crash fix for Vice City

--- GLAlphaFunc/main.cpp
+++ GLAlphaFunc/main.cpp
@@ -1,20 +1,29 @@
 #include <mod/amlmod.h>
-#include <mod/logger.h>
 
 #define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
 MYMOD(net.rusjj.glalphafunc, GLAlphaFunc CrashFix, 1.0, RusJJ)
-NEEDGAME(com.rockstargames.gtasa)
-BEGIN_DEPLIST()
-    ADD_DEPENDENCY_VER(net.rusjj.aml, 1.0)
-END_DEPLIST()
+//NEEDGAME(com.rockstargames.gtasa)
 
-uintptr_t pGTASA = 0;
+uintptr_t pGameLib = 0;
 extern "C" void OnModLoad()
 {
-    pGTASA = aml->GetLib("libGTASA.so");
-    aml->Unprot(pGTASA + 0x6BCBF8, sizeof(void*));
-    *(void**)(pGTASA + 0x6BCBF8) = (void*)glAlphaFuncQCOM;
+    pGameLib = aml->GetLib("libGTASA.so");
+    if(pGameLib)
+    {
+        aml->Unprot(pGameLib + 0x6BCBF8, sizeof(void*));
+        *(void**)(pGameLib + 0x6BCBF8) = (void*)glAlphaFuncQCOM;
+        return;
+    }
+
+    pGameLib = aml->GetLib("libGTAVC.so");
+    if(pGameLib)
+    {
+        aml->Unprot(pGameLib + 0x7300E0, sizeof(void*));
+        *(void**)(pGameLib + 0x7300E0) = (void*)glAlphaFuncQCOM;
+        return;
+    }
+    
 }
