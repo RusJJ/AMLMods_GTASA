42937cd5e448075a0f48834857d74d9cd196e66a
RusJJ <rusjj1337@gmail.com>
2021-08-15 09:19:50 +0300

    Perfectionism is good

--- PopcycleWorks/Android.mk
+++ PopcycleWorks/Android.mk
@@ -6,5 +6,5 @@ LOCAL_MODULE    := PopcycleWorks
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
-LOCAL_LDLIBS += -llog # ARM64 library requires it so...
+LOCAL_LDLIBS += -llog
 include $(BUILD_SHARED_LIBRARY)
--- PopcycleWorks/main.cpp
+++ PopcycleWorks/main.cpp
@@ -105,49 +105,49 @@ DECL_HOOK(void*, PopcycleInit, void* self, const char* a1)
     return ret;
 }
 
-extern "C" void OnModLoad() // A place for interfaces registering
+extern "C" void OnModLoad()
 {
     logger->SetTag("Popcycle Works");
     pGTASA = aml->GetLib("libGTASA.so");
 
-    cfg->Bind("ScaleOrOverride", "Override wont work on scaling", "A Info");
+    cfg->Bind("ScaleOrOverride", "Override wont work on scaling", "An Info");
 
-    pCfgScaleMaxNumPeds = cfg->Bind("MaxNumPeds", "0.0", "Scale");
-    pCfgScaleMaxNumCars = cfg->Bind("MaxNumCars", "0.0", "Scale");
-    pCfgScalePercDealers = cfg->Bind("PercDealers", "0.0", "Scale");
-    pCfgScalePercGang = cfg->Bind("PercGang", "0.0", "Scale");
-    pCfgScalePercCops = cfg->Bind("PercCops", "0.0", "Scale");
-    pCfgScalePercOther = cfg->Bind("PercOther", "0.0", "Scale");
+    pCfgScaleMaxNumPeds =               cfg->Bind("MaxNumPeds", "0.0", "Scale");
+    pCfgScaleMaxNumCars =               cfg->Bind("MaxNumCars", "0.0", "Scale");
+    pCfgScalePercDealers =              cfg->Bind("PercDealers", "0.0", "Scale");
+    pCfgScalePercGang =                 cfg->Bind("PercGang", "0.0", "Scale");
+    pCfgScalePercCops =                 cfg->Bind("PercCops", "0.0", "Scale");
+    pCfgScalePercOther =                cfg->Bind("PercOther", "0.0", "Scale");
 
     pCfgMaxNumPeds = cfg->Bind("MaxNumPeds", "-1", "OverrideNumber");
-    if(pCfgMaxNumPeds->GetInt() > 255) pCfgMaxNumPeds->SetInt(255);
+    if(pCfgMaxNumPeds->GetInt() > 255)  pCfgMaxNumPeds->SetInt(255);
     pCfgMaxNumCars = cfg->Bind("MaxNumCars", "-1", "OverrideNumber");
-    if(pCfgMaxNumCars->GetInt() > 255) pCfgMaxNumCars->SetInt(255);
+    if(pCfgMaxNumCars->GetInt() > 255)  pCfgMaxNumCars->SetInt(255);
     pCfgPercDealers = cfg->Bind("PercDealers", "-1", "OverrideNumber");
     if(pCfgPercDealers->GetInt() > 255) pCfgPercDealers->SetInt(255);
     pCfgPercGang = cfg->Bind("PercGang", "-1", "OverrideNumber");
-    if(pCfgPercGang->GetInt() > 255) pCfgPercGang->SetInt(255);
+    if(pCfgPercGang->GetInt() > 255)    pCfgPercGang->SetInt(255);
     pCfgPercCops = cfg->Bind("PercCops", "-1", "OverrideNumber");
-    if(pCfgPercCops->GetInt() > 255) pCfgPercCops->SetInt(255);
+    if(pCfgPercCops->GetInt() > 255)    pCfgPercCops->SetInt(255);
     pCfgPercOther = cfg->Bind("PercOther", "-1", "OverrideNumber");
-    if(pCfgPercOther->GetInt() > 255) pCfgPercOther->SetInt(255);
+    if(pCfgPercOther->GetInt() > 255)   pCfgPercOther->SetInt(255);
 
     cfg->Save();
 
-    CPopCycle_nPercTypeGroup = (char*)(pGTASA + 0x9F1664);
-    CPopCycle_nMaxNumPeds = (unsigned char*)(pGTASA + 0x9F0B24);
-    CPopCycle_nMaxNumCars = (unsigned char*)(pGTASA + 0x9F0D04);
-    CPopCycle_nPercDealers = (unsigned char*)(pGTASA + 0x9F0EE4);
-    CPopCycle_nPercGang = (unsigned char*)(pGTASA + 0x9F10C4);
-    CPopCycle_nPercCops = (unsigned char*)(pGTASA + 0x9F12A4);
-    CPopCycle_nPercOther = (unsigned char*)(pGTASA + 0x9F1484);
+    CPopCycle_nPercTypeGroup =  (char*)(pGTASA + 0x9F1664);
+    CPopCycle_nMaxNumPeds =     (unsigned char*)(pGTASA + 0x9F0B24);
+    CPopCycle_nMaxNumCars =     (unsigned char*)(pGTASA + 0x9F0D04);
+    CPopCycle_nPercDealers =    (unsigned char*)(pGTASA + 0x9F0EE4);
+    CPopCycle_nPercGang =       (unsigned char*)(pGTASA + 0x9F10C4);
+    CPopCycle_nPercCops =       (unsigned char*)(pGTASA + 0x9F12A4);
+    CPopCycle_nPercOther =      (unsigned char*)(pGTASA + 0x9F1484);
     
-    if( pCfgScaleMaxNumPeds->GetFloat() > 0.0f || pCfgMaxNumPeds->GetInt() >= 0 || 
-        pCfgScaleMaxNumCars->GetFloat() > 0.0f || pCfgMaxNumCars->GetInt() >= 0 || 
-        pCfgScalePercDealers->GetFloat() > 0.0f || pCfgPercDealers->GetInt() >= 0 || 
-        pCfgScalePercGang->GetFloat() > 0.0f || pCfgPercGang->GetInt() >= 0 || 
-        pCfgScalePercCops->GetFloat() > 0.0f || pCfgPercCops->GetInt() >= 0 || 
-        pCfgScalePercOther->GetFloat() > 0.0f || pCfgPercOther->GetInt() >= 0
+    if( pCfgScaleMaxNumPeds->GetFloat() > 0.0f  ||  pCfgMaxNumPeds->GetInt() >= 0  || 
+        pCfgScaleMaxNumCars->GetFloat() > 0.0f  ||  pCfgMaxNumCars->GetInt() >= 0  || 
+        pCfgScalePercDealers->GetFloat() > 0.0f ||  pCfgPercDealers->GetInt() >= 0 || 
+        pCfgScalePercGang->GetFloat() > 0.0f    ||  pCfgPercGang->GetInt() >= 0    || 
+        pCfgScalePercCops->GetFloat() > 0.0f    ||  pCfgPercCops->GetInt() >= 0    || 
+        pCfgScalePercOther->GetFloat() > 0.0f   ||  pCfgPercOther->GetInt() >= 0
     )
         HOOKPLT(PopcycleInit, pGTASA + 0x6734CC);
 }
