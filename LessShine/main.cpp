e16e0512d9e2d7718d4f90b1f82d07cc975296d6
RusJJ <rusjj1337@gmail.com>
2021-08-15 09:15:21 +0300

    Upd LessShine
    A little cleanup

--- LessShine/main.cpp
+++ LessShine/main.cpp
@@ -4,27 +4,24 @@
 
 MYMODCFG(net.rusjj.gtasa.lessshine, GTA:SA Less Ped Shining, 1.0, RusJJ)
 NEEDGAME(com.rockstargames.gtasa)
-BEGIN_DEPLIST()
-    ADD_DEPENDENCY_VER(net.rusjj.aml, 1.0)
-END_DEPLIST()
 
 uintptr_t pGTASA = 0;
 ConfigEntry* pShinessScale;
 
-unsigned char* pCTimeCycle_nDirectionalMult;
-DECL_HOOK(bool, CTimeCycle_Initialise, void* self, bool a1)
+unsigned char* pDirectionalMult;
+DECL_HOOK(bool, TimeCycleInit, void* self, bool a1)
 {
-    bool ret = CTimeCycle_Initialise(self, a1);
+    bool ret = TimeCycleInit(self, a1);
     /*for(int hour = 0; hour < 24; ++hour)
     {
         for(int weatherType = 0; weatherType < 0; ++weatherType)
         {
-            ((char*)(pCTimeCycle_nDirectionalMult + 23*hour))[weatherType] = (unsigned char)pShinessScale->GetInt();
+            ((char*)(nDirectionalMult + 23*hour))[weatherType] = (unsigned char)pShinessScale->GetInt();
         }
     }*/
     for(int i = 0; i < 184; ++i)
     {
-        pCTimeCycle_nDirectionalMult[i] = (unsigned char)pShinessScale->GetInt();
+        pDirectionalMult[i] = (unsigned char)pShinessScale->GetInt();
     }
     return ret;
 }
@@ -36,6 +33,6 @@ extern "C" void OnModLoad()
 
     pShinessScale = cfg->Bind("ShinessScale", "30");
 
-    pCTimeCycle_nDirectionalMult = (unsigned char*)(pGTASA + 0x968A18);
-    HOOKPLT(CTimeCycle_Initialise, pGTASA + 0x67489C);
+    pDirectionalMult = (unsigned char*)(pGTASA + 0x968A18);
+    HOOKPLT(TimeCycleInit, pGTASA + 0x67489C);
 }
